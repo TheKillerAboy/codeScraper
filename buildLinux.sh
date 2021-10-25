2492ab0bde81df3cec1b875c3fa063628555f4c4
TheKillerAboy <aboy001@outlook.com>
2021-10-25 03:06:41 -0500

    backup

--- buildLinux.sh
+++ buildLinux.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 
 /home/kalilaptop/Documents/GitHub/codeScraper/venv/bin/python3.8 -m PyInstaller /home/kalilaptop/Documents/GitHub/codeScraper/codeScraperLinux.spec -y
-/home/kalilaptop/Documents/GitHub/codeScraper/install.sh
