28390ab47c697ec3c71d8f0e0098ea064121c920
Annekin Meyburgh <annekin.meyburgh@takealot.com>
2021-11-25 23:22:56 +0200

    remove python

--- myvenvmake.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-rm -rf /venv
-which python3.8
-python3.8 -m venv venv
-/home/kalilaptop/Documents/GitHub/codeScraper/venv/bin/python3.8 -m pip install --upgrade pip
-/home/kalilaptop/Documents/GitHub/codeScraper/venv/bin/pip3 install -r requirements.txt
